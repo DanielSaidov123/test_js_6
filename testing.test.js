3d1b8a7861fed748c22a31017c3dc20898e9219e
DanielSaidov123 <ddny3622@gmail.com>
2026-01-08 11:29:44 +0200

    add testing

---
+++ testing.test.js
@@ -0,0 +1,16 @@
+import assert from "assert";
+import { test } from "node:test";
+import { Student } from "./classes/student.js";
+import { Flight } from "./classes/flight.js";
+
+test(" Student Class", () => {
+  const flay = new Flight("daniel", "fdsf", 132, 3, 100, 200);
+  const student1 = new Student("ff", 23, 1, "vewgg");
+  assert.strictEqual(student1.Purchasingtickets(flay,'VIP'), false);
+});
+test(" Student Class", () => {
+  const flay = new Flight("daniel", "fdsf", 132, 3, 100, 200);
+  const student1 = new Student("ff", 23, 1000, "vewgg");
+    const ticket =student1.Purchasingtickets(flay,'VIP')
+  assert.deepStrictEqual(ticket, [200,'ff'] );
+});
