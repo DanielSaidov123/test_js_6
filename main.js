9e9155b44e9248310e12286ced09f007d96ac06b
DanielSaidov123 <ddny3622@gmail.com>
2026-01-08 12:31:14 +0200

    add main and more testing

---
+++ main.js
@@ -0,0 +1,29 @@
+import { Flight } from "./classes/flight.js";
+import { RegularPassenger } from "./classes/regularPerson.js";
+import { Student } from "./classes/student.js";
+
+
+
+
+
+const flay = new Flight("daniel", "fdsf", 132, 3, 100, 200);
+const student1 = new Student("ff", 23, 1000, "vewgg");
+const student2 = new Student("dani", 23, 1000, "vewgg");
+const regular1 = new RegularPassenger("dani", 23, 1000, "fewfwa", true);
+const regular4 = new RegularPassenger("dani", 23, 1000, "fewfwa", false);
+const data1 = student1.Purchasingtickets(flay, "regolar");
+const dat2 = student2.Purchasingtickets(flay, "VIP");
+const data3 = regular1.Purchasingtickets(flay, "regolar");
+const data4 = regular4.Purchasingtickets(flay, "VIP");
+
+const ticket1 = flay.createTicketRegular(data1);
+const ticket2 = flay.createTicketRegular(dat2);
+const ticket3= flay.createTicketRegular(data3);
+const ticket4 = flay.createTicketRegular(data4);
+
+flay.addtoarr(ticket1);
+flay.addtoarr(ticket2);
+flay.addtoarr(ticket3);
+flay.addtoarr(ticket4);
+
+console.log(flay);
--- testing.test.js
+++ testing.test.js
@@ -2,15 +2,40 @@ import assert from "assert";
 import { test } from "node:test";
 import { Student } from "./classes/student.js";
 import { Flight } from "./classes/flight.js";
+import { RegularPassenger } from "./classes/regularPerson.js";
 
-test(" Student Class", () => {
+test(" TESTING Class", () => {
   const flay = new Flight("daniel", "fdsf", 132, 3, 100, 200);
   const student1 = new Student("ff", 23, 1, "vewgg");
-  assert.strictEqual(student1.Purchasingtickets(flay,'VIP'), false);
+  assert.strictEqual(student1.Purchasingtickets(flay, "VIP"), false);
 });
-test(" Student Class", () => {
+test(" TESTING Class", () => {
   const flay = new Flight("daniel", "fdsf", 132, 3, 100, 200);
   const student1 = new Student("ff", 23, 1000, "vewgg");
-    const ticket =student1.Purchasingtickets(flay,'regolar')
-  assert.deepStrictEqual(ticket, [90,'ff'] );
+  const ticket = student1.Purchasingtickets(flay, "regolar");
+  assert.deepStrictEqual(ticket, [90, "ff"]);
+});
+test(" TESTING Class", () => {
+  const flay = new Flight("daniel", "fdsf", 132, 3, 100, 200);
+  const student1 = new Student("ff", 23, 1000, "vewgg");
+  const ticket = student1.Purchasingtickets(flay, "VIP");
+  assert.deepStrictEqual(ticket, [200, "ff"]);
+});
+test(" TESTING Class", () => {
+  const flay = new Flight("daniel", "fdsf", 132, 3, 100, 200);
+  const regular = new RegularPassenger("ff", 23, 1000, "vewgg", true);
+  const ticket = regular.Purchasingtickets(flay, "VIP");
+  assert.deepStrictEqual(ticket, [85, "ff"]);
+});
+test(" TESTING Class", () => {
+  const flay = new Flight("daniel", "fdsf", 132, 3, 100, 200);
+  const regular = new RegularPassenger("ff", 23, 1000, "vewgg", true);
+  const ticket = regular.Purchasingtickets(flay, "regolar");
+  assert.deepStrictEqual(ticket, [80, "ff"]);
+});
+test(" TESTING Class", () => {
+  const flay = new Flight("daniel", "fdsf", 132, 3, 100, 200);
+  const regular = new RegularPassenger("ff", 23, 1, "vewgg", true);
+  const ticket = regular.Purchasingtickets(flay, "regolar");
+  assert.deepStrictEqual(ticket, false);
 });
